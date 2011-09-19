8bde21e2e28bbdf9f1c9c595254238c1891c70d5
jimmy hendrix <jimmythedestroyer@gmail.com>
2011-09-18 20:48:42 -0700

    click to color pixels

--- pointAndPixel/PixelCanvas.java
+++ pointAndPixel/PixelCanvas.java
@@ -10,6 +10,8 @@ import java.awt.event.*;
 import java.io.*;
 
 public class PixelCanvas extends JPanel {
+  
+  private JFrame frame = null;
 
   private boolean gridOn = true;
   
@@ -17,12 +19,14 @@ public class PixelCanvas extends JPanel {
   private int heightPixels = 60;
   private int widthPixels = 60;
   
-  private JFrame frame = null;
+  private Color selectedColor = new Color(0, 0, 0, 255);
+  
+  private Color[][] grid = new Color[widthPixels][heightPixels];
 
   public PixelCanvas(JFrame frame) {
     this.frame = frame;
   
-    addMouseListener(new MAdapter());
+    addMouseListener(new MAdapter(this));
     setFocusable(true);
     
     setBackground(Color.WHITE);
@@ -30,6 +34,15 @@ public class PixelCanvas extends JPanel {
     frame.setSize((int)((widthPixels + 0.5) * pixelSize), (int)((heightPixels + 0.5) * pixelSize));
     
     setVisible(true);
+  }
+  
+  public void colorPixelFromClick(int x, int y) {
+    colorPixel((int)(x / pixelSize), (int)(y / pixelSize));
+  }
+  
+  public void colorPixel(int column, int row) {
+    grid[column][row] = selectedColor;
+    repaint();
   }
   
   public void paint(Graphics g) {
@@ -45,13 +58,34 @@ public class PixelCanvas extends JPanel {
         g2d.drawLine(0, y, widthPixels * pixelSize, y);
       }
     }
+    
+    for (int column = 0; column < widthPixels; column++) {
+      for (int row = 0; row < heightPixels; row++) {
+        if (grid[column][row] != null) {
+          Color c = g2d.getColor();
+          
+          g2d.setColor(grid[column][row]);
+          g2d.fillRect(column * pixelSize, row * pixelSize, pixelSize, pixelSize);
+          
+          g2d.setColor(c);
+        }
+      }
+    }
   }
 
   class MAdapter extends MouseAdapter 
 	{
+	  private PixelCanvas canvas = null;
+	  
+	  public MAdapter(PixelCanvas canvas) {
+	    this.canvas = canvas;
+	  }
+	  
     public void mousePressed(MouseEvent e) {
-      //int x = e.getX();
-      //int y = e.getY();
+      int x = e.getX();
+      int y = e.getY();
+      
+      canvas.colorPixelFromClick(x, y);
     }
 
     public void mouseReleased(MouseEvent e) {
