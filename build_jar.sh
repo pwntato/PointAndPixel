8819949a77376d36b3e9a408ff7f1d7022b9293b
jimmy hendrix <jimmythedestroyer@gmail.com>
2011-10-11 19:38:57 -0700

    broke down and included the jar in the build

--- build_jar.sh
+++ build_jar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #compile
-javac PointAndPixel.java pointAndPixel/*.java
-jar cfm ../PointAndPixel.jar Manifest.txt PointAndPixel.class pointAndPixel/*.class
+javac PointAndPixel.java pointAndPixel/*.java && jar cfm PointAndPixel.jar Manifest.txt PointAndPixel.class pointAndPixel/*.class && java -jar PointAndPixel.jar 
+
 
